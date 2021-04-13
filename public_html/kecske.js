cf1bb29e0447fc4b74506c02a5e55871de1fb7b9
Katona Janos <katonajanos0128@gmail.com>
2021-04-13 12:59:43 +0200

    1.feladat

---
+++ public_html/kecske.js
@@ -0,0 +1,13 @@
+window.addEventListener("load",init);
+
+
+function ID(nev){
+    return document.getElementById(nev);
+}
+function $(nev){
+    return document.querySelectorAll(nev);
+}
+function init (){
+    
+    
+}
