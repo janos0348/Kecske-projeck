3ad7f5622ed49e81b4a3059e8f35484dabf60f2a
Katona Janos <katonajanos0128@gmail.com>
2021-04-13 13:48:10 +0200

    1,2,4,5,6 Feladat

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -1,13 +1,31 @@
-window.addEventListener("load",init);
+window.addEventListener("load", init);
 
 
-function ID(nev){
+function ID(nev) {
     return document.getElementById(nev);
 }
-function $(nev){
+function $(nev) {
     return document.querySelectorAll(nev);
 }
-function init (){
-    
-    
+function init() {
+    var kepElemCim = document.querySelectorAll("img");
+    var kepElem = $("img");
+    for (var i = 0; i < kepElem.length; i++) {
+        kepElem[i].addEventListener("mouseover", formazFel);
+        kepElem[i].addEventListener("mouseout", formazLe);
+    }
+
+    document.title = 'Kovács Dániel Csaba, Bogdán Gábor, Katona János ';
+            var eleresek = new array;
+    eleresek.push(this.src);
+
+
+}
+
+function formazFel() {
+    this.classList.add("kiemel");
+}
+
+function formazLe() {
+    this.classList.remove("kiemel");
 }
